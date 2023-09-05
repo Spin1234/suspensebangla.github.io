1fe89ad2935d238da70028e3634d71ad2dede352
Sayak Pal <121346103+Spin1234@users.noreply.github.com>
2023-09-05 21:48:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ async function fetchplaylist(nextPageToken){
     const cardsCont = document.getElementById("cards-cont");
     const template = document.getElementById("tem");
 
- 
+
     let results = data.items;
 
     results.forEach((item) => {
@@ -65,4 +65,73 @@ document.getElementById("loadmore").addEventListener('click', async ()=>{
 })
 
 
+
+// search functionality
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("Search");
+let keyword = "";
+
+async function searchKey(nextPageToken){
+
+    
+
+    keyword = searchBox.value;
+    const url1 =  `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCmzj6hXrPZ_AwIZ8lgo-HuQ&maxResults=25&pageToken=${nextPageToken}&q=${keyword}&key=${api}`;
+    const response = await fetch(url1);
+    const data = await response.json();
+
+    if (data.pageInfo.totalResults === 0) {
+        window.alert("no results found!")
+        return;
+    }
+
+    document.getElementById("tag").innerHTML = "Search reaults for: "+keyword;
+
+    const results1 = data.items;
+
+    results1.forEach(item1 => {
+
+        const cardsCont = document.getElementById("cards-cont");
+        const template = document.getElementById("tem");
+
+
+        if (item1.snippet && item1.snippet.thumbnails && item1.snippet.thumbnails.medium && item1.snippet.thumbnails.medium.url && item1.id.videoId) {
+            
+            const img = item1.snippet.thumbnails.medium.url;
+            
+            const cardClone = template.content.cloneNode(true);
+    
+            const backImg = cardClone.getElementById("im");
+            backImg.style.backgroundImage = `url(${img})`;
+    
+            const title = cardClone.getElementById("tle");
+            title.innerHTML = item1.snippet.title;
+    
+            const btn = cardClone.getElementById("link");
+            btn.href = "play.html?id="+item1.id.videoId+"&img="+item1.snippet.thumbnails.medium.url;
+    
+            cardsCont.appendChild(cardClone);
+            }
+    });
+    
+
+    nextPageToken1=data.nextPageToken;
+    return data.nextPageToken;
+
+}
+
+searchForm.addEventListener("submit", (e)=> {
+    
+    e.preventDefault();
+    const cardsCont = document.getElementById("cards-cont");
+    cardsCont.innerHTML = "";
+    nextPageToken = "";
+    searchKey(nextPageToken);
+})
+
+document.getElementById("loadmore").addEventListener("click", async(e)=>{
+    e.preventDefault();
+    nextPageToken = await searchKey(nextPageToken1);
+})
 
